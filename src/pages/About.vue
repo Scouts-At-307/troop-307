26ece068c08333b753ff9115373a5aeeb34956db
linux-overuser <michaelhthomas@outlook.com>
2020-04-29 11:08:56 -0400

    Update About page template

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,29 @@
 <template>
-  <Layout>
-    This is just a basic 'About Us' page!
-  </Layout>
+	<Layout>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-10 is-offset-1">
+            <h1 class="title is-1">{{ title }}</h1>
+            <p>This is just a basic 'About Us' page!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+	</Layout>
 </template>
 
 <script>
 export default {
-  metaInfo: {
-    title: 'About Us',
+  data() {
+    return {
+      title: 'About Us'
+    }
   },
+	metaInfo() {
+    return {
+      title: this.title,
+    }
+	},
 };
 </script>
