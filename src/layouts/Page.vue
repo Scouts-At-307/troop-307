2e67224e307b4655d0101358360499be70259a93
linux-overuser <michaelhthomas@outlook.com>
2020-04-30 11:36:32 -0400

    Update foudational templates, add fontawesome

---
+++ src/layouts/Page.vue
@@ -0,0 +1,23 @@
+<template>
+    <Layout>
+        <section class="section">
+        <div class="container">
+            <div class="columns">
+            <div class="column is-10 is-offset-1">
+                <slot />
+            </div>
+            </div>
+        </div>
+        </section>
+	</Layout>
+</template>
+
+<script>
+import Layout from '~/layouts/Default.vue'
+
+export default {
+  components: {
+    Layout
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 // Import main css
 import '~/assets/style/index.scss';
 
+// // Fontawesome
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { fas } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+// library.add(fas);
+
+
+
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from '~/layouts/Default.vue';
+import Page from '~/layouts/Page.vue';
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
+  Vue.component('Page', Page);
+
+  // Fontawesome
+  // Vue.component('font-awesome-icon', FontAwesomeIcon)
 }
