4ba83ca44d6ffdcb36563e816330730011c0fa48
linux-overuser <michaelhthomas@outlook.com>
2020-05-01 19:11:23 -0400

    Fix publicPath for Netlify CMS

--- gridsome.config.js
+++ gridsome.config.js
@@ -8,8 +8,7 @@ module.exports = {
   siteName: 'Troop 307',
   siteDescription: 'Boy Scout Troop chartered by Saxe Gotha Presbyterian Church in Lexington, South Carolina.',
 
-  siteUrl: 'https://scoutsat307.github.io',
-  pathPrefix: '/troop-307',
+  siteUrl: 'https://michael.tru.io',
 
   templates: {
     Post: '/:title',
