c79c28b2a4b5744beaff6e6f3959fc41eb602b4d
linux-overuser <michaelhthomas@outlook.com>
2020-05-06 16:07:01 -0400

    Update 404 Page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,8 +1,20 @@
 <template>
   <Page>
-    <h1 class="is-size-2">
-      Error: 404
-    </h1>
+    <div class="has-text-centered">
+      <h1 style="font-size: 6rem;">
+        404
+      </h1>
+      <h2 class="subtitle is-size-2">
+        That's an error.
+      </h2>
+      <h4 class="is-size-4">
+        The page that you think should be here, isn't here.
+      </h4>
+      <br />
+      <g-link to="/" class="button is-secondary">
+        Go Home
+      </g-link>
+    </div>
   </Page>
 </template>
 
